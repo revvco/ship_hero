f3c37b8cc8c3539dd1256a58a244fdcc0c793c4e
Kyle Schutt <kyle@revv.co>
2018-05-23 11:32:37 -0400

    uncomment shipping carrier

--- lib/ship_hero/models/order_shipping_lines.rb
+++ lib/ship_hero/models/order_shipping_lines.rb
@@ -2,7 +2,7 @@ module ShipHero
   class OrderShippingLines < ShipHero::Base
     property :title, coerce: String
     property :price, coerce: String
-    # property :method, coerce: String
+    property :method, coerce: String
     # property :carrier, coerce: String
   end
 end
