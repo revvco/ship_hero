af49eb497c2f770ebb4585ef5dda68f3958cdcb6
tirdadc <tirdadck@gmail.com>
2020-11-13 10:12:00 -0500

    Updated GetProductQuery

--- lib/ship_hero/graphql/queries/get_product_query.rb
+++ lib/ship_hero/graphql/queries/get_product_query.rb
@@ -4,6 +4,9 @@ module ShipHero
       query($sku: String) {
         product(sku: $sku) {
           data {
+            id
+            legacy_id
+            account_id
             price
             value
             name
