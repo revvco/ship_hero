27626fa9387a2f66466ceba4b29de1e5e3dbf10d
tirdadc <tirdadck@gmail.com>
2020-11-20 17:23:53 -0500

    Updated response for create_order

--- lib/ship_hero/services/order_service.rb
+++ lib/ship_hero/services/order_service.rb
@@ -19,7 +19,8 @@ module ShipHero
 
       def create_order(request)
         raise Exceptions::ServiceException, "Must be a ShipHero::Order" unless request.is_a?(ShipHero::Order)
-        client.query ShipHero::Queries::CreateOrderQuery, { newOrder: request }
+        response = client.query ShipHero::Queries::CreateOrderQuery, { newOrder: request }
+        ShipHero::Responses::CreateOrder(order_id: response&.data&.order_create&.order.id)
       end
 
       def update_order(request)
