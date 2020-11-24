714ea16c41d2ddd6a1ce413cc9db9070a2360494
tirdadc <tirdadck@gmail.com>
2020-11-24 10:24:54 -0500

    source no longer exists for webhook

--- lib/ship_hero/graphql/queries/register_webhook_query.rb
+++ lib/ship_hero/graphql/queries/register_webhook_query.rb
@@ -4,6 +4,10 @@ module ShipHero
     mutation($newWebhook: CreateWebhookInput!) {
       webhook_create(data: $newWebhook) {
         request_id
+        webhook {
+          name
+          url
+        }
       }
     }
     GRAPHQL
